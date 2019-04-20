79a11b99d387441e7b65a2e5d1f998f030834a69
elvis <yinchuang94@gmail.com>
2019-04-21 02:47:20 +0800

    wip: add first endpoint

--- lib/binance_chain_ruby.rb
+++ lib/binance_chain_ruby.rb
@@ -1,6 +1,7 @@
 require "binance_chain_ruby/version"
 
 module BinanceChainRuby
-  class Error < StandardError; end
-  # Your code goes here...
+  # to start with ticker and market endpoint
+  #test this url
+  # https://testnet-dex.binance.org/api/v1/markets
 end
