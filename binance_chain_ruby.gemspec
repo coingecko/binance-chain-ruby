b683e036dd3e86d9cdf5fbd9b894b84167a77dbb
elvis <yinchuang94@gmail.com>
2019-04-21 19:48:36 +0800

    implemented first endpoint

--- binance_chain_ruby.gemspec
+++ binance_chain_ruby.gemspec
@@ -39,4 +39,8 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.17"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "http"
+  spec.add_development_dependency "pry"
+  spec.add_development_dependency "vcr"
+  spec.add_development_dependency "webmock"
 end
--- lib/binance_chain_ruby.rb
+++ lib/binance_chain_ruby.rb
@@ -1,7 +1,17 @@
 require "binance_chain_ruby/version"
+require "http"
+require "yaml"
 
 module BinanceChainRuby
-  # to start with ticker and market endpoint
-  #test this url
-  # https://testnet-dex.binance.org/api/v1/markets
+  class Client
+    def tickers
+      api_url = "https://testnet-dex.binance.org/api/v1/ticker/24hr"
+      output = JSON.parse(HTTP.timeout(:write => 2, :connect => 5, :read => 8).get(api_url))
+      output
+      
+      # to get all tickers from binance chain
+      # client = BinanceChainRuby::Client.new
+      # client.tickers
+    end
+  end
 end
